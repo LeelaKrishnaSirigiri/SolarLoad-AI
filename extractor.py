1cf7e3d83ee65173d1d7876f38e47ac9621e8347
Sirigiri Leela Krishna Reddy <sirigirileelakrishna1133@gmail.com>
2026-05-04 18:45:57 +0530

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 
 
 def load_image(uploaded_file):
